84d2d18b310fa4ab95c0a8299176afd119913a2b
CRFjonathas <jonathas.github@gmail.com>
2025-11-03 15:35:08 -0300

    feat: aprimora a exibição das listas lineares e adiciona realocação de memória

--- C/ListasLineares.c
+++ C/ListasLineares.c
@@ -4,7 +4,9 @@
 int main(){
     // LISTA LINEAR ESTÁTICA
     int numeros[5] = {10, 20, 30, 40, 50};
-    printf("%d", numeros[2]);
+
+    printf("LISTA LINEAR ESTÁTICA\n");
+    printf("%d\n\n", numeros[2]);
 
     //LISTA LINEAR DINÂMICA
     int* num = (int*) malloc(3 * sizeof(int)); // ALOCANDO MEMORIA INICIAL
@@ -13,14 +15,17 @@ int main(){
     num[1] = 20;
     num[2] = 30; 
 
+    printf("LISTA LINEAR DINÂMICA (ALOCAÇÃO DE MEMORIA INICIAL)\n");
     printf("%d, ", num[0]);
     printf("%d, ", num[1]);
     printf("%d, ", num[2]);
+
     num = (int*) realloc(num, 5 * sizeof(int)); // REALOCANDO A MEMORIA PARA EXPANDIR A LISTA
 
-    numeros[3] = 40;
-    numeros[4] = 50; 
+    num[3] = 40;
+    num[4] = 50; 
 
+    printf("LISTA LINEAR DINÂMICA (REALOCAÇÃO DE MEMORIA INICIAL)\n");
     printf("\n\n%d, ", num[3]);
-    printf("%d, ", num[4]);
+    printf("%d ", num[4]);
 }
