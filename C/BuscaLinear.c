699b565a1a56004527c01e266e6c7e69cb5506f0
CRFjonathas <jonathas.github@gmail.com>
2025-11-06 16:07:40 -0300

    feat: adiciona implementação da busca linear em array e lista encadeada

---
+++ C/BuscaLinear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int buscaLinear(int lista[], int tamanho, int valor) {
+    for (int i = 0; i < tamanho; i++) {
+        if (lista[i] == valor) {
+            return i; // Retorna o índice onde encontrou o valor
+        }
+    }
+    return -1; // Retorna -1 se não encontrou
+} 
+
+int buscaLinearLista(struct No* inicio, int valor) {
+    int pos = 0;
+    struct No* atual = inicio;
+    while (atual != NULL) {
+        if (atual->dado == valor) {
+            return pos;
+        }
+        atual = atual->proximo;
+        pos++;
+    }
+    return -1;
+} 
